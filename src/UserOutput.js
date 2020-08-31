8dec56d7332e0f5fa418de40fea7ddf1cab4ee33
Yoonsoo <ypark9@ncsu.edu>
2020-08-31 18:10:10 -0400

    first commit

---
+++ src/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import userInput from './UserInput';
+
+const userOutput = (props) => {
+	return (
+		<div>
+			<p>User Name : {props.userName}</p>
+			<p>paragraph2</p>
+		</div>
+	);
+};
+
+export default userOutput;
